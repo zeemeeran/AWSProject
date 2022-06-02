8a27736196c758a3941874288f91c337c591a078
zeemeeran <100310293+zeemeeran@users.noreply.github.com>
2022-06-02 16:05:08 -0400

    Update appinit.py

--- appinit.py
+++ appinit.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://********.amazonaws.com:5432/studentdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@********.amazonaws.com:5432/studentdb'
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
