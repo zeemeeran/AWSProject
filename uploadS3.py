da66bb28771a5c6d392d216b62d29b6860f44401
Zahida Meeran <dev.zmeeran@gmail.com>
2022-06-02 14:50:27 -0400

    Updating up s3 connect code

--- uploadS3.py
+++ uploadS3.py
@@ -3,8 +3,21 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
+
+
+# #Creating Session With Boto3.
+# session = boto3.Session(
+# aws_access_key_id='<your_access_key_id>',
+# aws_secret_access_key='<your_secret_access_key>'
+# )
+
+
+# s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                   #   aws_secret_access_key=SECRET_KEY)
+
+
 def upload(fname):
-    s3_client = boto3.client('s3')
+    s3_client = boto3.client('s3', aws_access_key_id = 'AKIA2ZDMZCD6WQS3DAS7', aws_secret_access_key='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')
     path = 'static/files/' + fname
     try:
         response = s3_client.upload_file(path, 'student-pictures', fname)
